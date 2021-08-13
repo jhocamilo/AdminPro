6a3949aa90b760963cc41d2fccb0e5e05ac92ce5
Camilo Giraldo <johangiraldo@unicomfacauca.edu.co>
2021-08-12 23:44:46 -0500

    seccion 3 - diseño

---
+++ src/app/pages/nopagesfound/nopagesfound.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagesfound',
+  templateUrl: './nopagesfound.component.html',
+  styleUrls: [ './nopagefound.component.css'
+  ]
+})
+export class NopagesfoundComponent implements OnInit {
+
+  year = new Date().getFullYear();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/slidebar/slidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slidebar',
+  templateUrl: './slidebar.component.html',
+  styles: [
+  ]
+})
+export class SlidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
